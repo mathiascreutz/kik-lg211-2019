90f2ff970f1fd1eb7240a8d06d66185d7fcd0504
Mathias Creutz <mathias.creutz@alpha-sense.com>
2019-10-24 19:37:38 +0300

    Setup and Tf-idf demo

---
+++ bootstrap.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/jovyan/
+
+rm -r work
+rm bootstrap.bash
+
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+git clone https://github.com/mathiascreutz/kit-lg211-2019.git
+
+pip install nltk
+python -c "import nltk; nltk.download(['gutenberg'])"
+
+#, 'genesis', 'treebank', 'nps_chat', 'inaugural', 'webtext', 'wordnet', 'punkt', 'averaged_perceptron_tagger', 'brown', 'tagsets'])"
+
+#cd computational-semantics/assignment5/data/quora/
+#gunzip quora_duplicate_questions.tsv.gz
+
+#cd /home/jovyan/
